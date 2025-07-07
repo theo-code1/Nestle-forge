87eb5f02ab5e0c33f67e48264db21989e446aa52
theo-code1 <codingbusiness99@gmail.com>
2025-07-07 18:55:36 +0100

    Add background removal feature with Flask backend and UI integration
    Introduces a new Flask route for background removal, allowing users to upload images and receive processed versions with backgrounds removed.
    
    Also adds a BgRemoveSection component to the frontend for user interaction, enhancing the overall image processing capabilities of the application.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import UpscalerSection from './Components/UpscalerSection'
 import ConverterSection from './Components/ConverterSection'
 import CompressorSection from './Components/CompressorSection'
+import BgRemoverSection from './Components/BgRemoveSection'
 import NotFound from './Components/NotFound'
 import RootLayout from './Layout/Rootlayout'
 
@@ -9,14 +10,13 @@ import RootLayout from './Layout/Rootlayout'
 
 const App = () => {
 
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<RootLayout />}>
         <Route index element={<UpscalerSection />} />
         <Route path='image-converter' element={<ConverterSection />} />
         <Route path='image-compressor' element={<CompressorSection />} />
-        {/* <Route path='background-remover' element={<BgRemoverSection />} /> */}
+        <Route path='background-remover' element={<BgRemoverSection />} />
         {/* <Route path='image-resizer-cropper' element={<ImgResizerSection />} /> */}
         {/* <Route path='color-palette-extractor' element={<PaletteExSection />} /> */}
         <Route path='*' element={<NotFound />} />
---
+++ src/Components/BgRemoveSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BgRemoveSection = () => {
+  return (
+    <section className='flex flex-col items-center gap-8 py-16 overflow-y-auto overflow-x-hidden max-h-[90dvh]'>
+        <div className="headers">
+            <h1>Remove Background from you images</h1>
+            <h3>Instantly erase backgrounds to create clean, transparent images—perfect for design, profiles, or web use.</h3>
+        </div>
+
+    </section>
+  )
+}
+
+export default BgRemoveSection
