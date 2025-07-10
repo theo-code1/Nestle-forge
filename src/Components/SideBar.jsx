34ce5c32786901b8e96fb1382c69515c3962dae2
theo-code1 <codingbusiness99@gmail.com>
2025-07-10 13:58:09 +0100

    Update SideBar layout and styles, enhance NestleForgeLogo component, and modify CSS variables
    Refines the SideBar component by adjusting layout properties, adding a logo with specified dimensions, and improving the hover effects on navigation links. Updates the NestleForgeLogo component to accept customizable width, height, and color props for better flexibility. Introduces a new CSS variable for blue color in index.css to enhance theming.

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -4,16 +4,19 @@ import NestleForgeLogo from './Icons/NestleForgeLogo.jsx'
 const SideBar = () => {
 
   return (
-    <main className='side-bar float-left w-1/7 h-screen border-r border-r-black/20 text-left py-8 px-4 flex flex-col gap-16 font-'>
-      <div className="logo">
-        <NestleForgeLogo />
+    <main className='side-bar float-left w-[20 h-screen border-r border-r-black/20 text-left py-6 px-2 flex flex-col gap-8 font-'>
+      <div className="logo flex items-center gap-2 px-2 mb-2">
+        <NestleForgeLogo width="32" height="32" color="black" className="" />
+        <h1 className="logo-name text-lg font-medium">NestleForge</h1>
       </div>
-        <ul className='text-sm flex flex-col gap-2 font-medium'>
-              <NavLink to='/'> <li className={`block hover:bg-gray-100 p-2 cursor-pointer rounded-lg `}>Image Converter</li> </NavLink>
-              <NavLink to='/image-compressor'> <li className={`block hover:bg-gray-100 p-2 cursor-pointer rounded-lg `}>Image Compressor</li> </NavLink>
-              <NavLink to='/background-remover'> <li className={`block hover:bg-gray-100 p-2 cursor-pointer rounded-lg `}>Background Remover</li> </NavLink>
-              <NavLink to='/color-palette-extractor'> <li className={`block hover:bg-gray-100 p-2 cursor-pointer rounded-lg `}>Color Palette Extractor</li> </NavLink>
 
+      <hr className='opacity-50 w-9/10 mx-auto'/>
+
+        <ul className='text-sm flex flex-col gap-1 font-medium'>
+              <NavLink to='/'> <li className={`block hover:bg-gray-50 p-2 cursor-pointer rounded-md `}>Image Converter</li> </NavLink>
+              <NavLink to='/image-compressor'> <li className={`block hover:bg-gray-50 p-2 cursor-pointer rounded-md `}>Image Compressor</li> </NavLink>
+              <NavLink to='/background-remover'> <li className={`block hover:bg-gray-50 p-2 cursor-pointer rounded-md `}>Background Remover</li> </NavLink>
+              <NavLink to='/color-palette-extractor'> <li className={`block hover:bg-gray-50 p-2 cursor-pointer rounded-md `}>Color Palette Extractor</li> </NavLink>
             
         </ul>
     </main>
