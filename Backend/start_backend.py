b8916fc68bc1f85f2416f4d02f1233803bdb1d7f
theo-code1 <codingbusiness99@gmail.com>
2025-07-23 20:06:41 +0100

    Configures backend for deployment and updates dependencies
    Updates the backend to be deployable by:
    
    -   Setting the application to listen on all interfaces
    -   Using the PORT environment variable to set the listening port
    -   Disabling debug mode in production
    -   Updating the requirements.txt file with the latest dependencies.

--- Backend/start_backend.py
+++ Backend/start_backend.py
@@ -146,4 +146,5 @@ def get_config_endpoint():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=False, host="0.0.0.0", port=port)
